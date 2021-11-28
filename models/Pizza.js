0537535133b13444fb185e3c6ba1d12471c285df
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-27 22:29:22 -0800

    add comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,16 +10,36 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
       type: String,
       default: 'Large'
     },
     toppings: []
-  });
+    ,
+    comments: [
+      {
+        type: String,
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+  );
 
-  // create the Pizza model using the PizzaSchema
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
